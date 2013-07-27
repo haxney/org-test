719580f9e9d3bf973f098a569f84670770e6b127
Daniel Hackney <dan@haxney.org>
2013-07-27 13:53:06 -0400

    Use a macro to capture most code in elisp file.

--- org-test.el
+++ org-test.el
@@ -1,20 +1,35 @@
-(with-current-buffer "test.org"
-  (save-excursion
-    (let (actual)
-      (outline-next-visible-heading)
-      (org-mark-subtree)
-      (setq actual (buffer-substring (point) (mark)))
-      (with-temp-buffer
-        (insert actual)
-        (search-forward "⁁")
-        (delete-char -1)
-        (org-mode)
-        )
+(require 'org)
+(require 'ert)
 
-      (outline-next-visible-heading)
-      (org-mark-subtree)
-      (setq expected (buffer-substring (point) (mark)))
-      (with-current-buffer expected-buf
-        (insert expected))
+;; org-babel-next-src-block
 
-      )))
+(defmacro org-test-def (name desc &rest body)
+  "Create an ERT test embedded in an org file."
+  (declare (indent 1))
+  `(save-excursion
+     (let (actual expected)
+       (outline-next-heading)
+       (org-mark-subtree)
+       (forward-line) ;; Don't include the actual headline
+       (setq actual (buffer-substring (point) (mark)))
+       (with-temp-buffer
+         (insert actual)
+         (goto-char (point-min))
+         (search-forward "⁁")
+         (delete-char -1)
+         (org-mode)
+         ,@body
+         (setq actual (buffer-substring (point-min) (point-max)))
+         )
+
+       (outline-next-heading)
+       (org-mark-subtree)
+       (forward-line)
+       (setq expected (buffer-substring (point) (mark)))
+       (with-temp-buffer
+         (insert expected)
+         (goto-char (point-min))
+         (search-forward "⁁")
+         (delete-char -1)
+         (setq expected (buffer-substring (point-min) (point-max))))
+       (string-equal expected actual))))
